51a9b3f273af6680dcdfe6368e848682e8d4a1d7
BuildTools <unconfigured@null.spigotmc.org>
2021-10-20 11:39:58 +0100

    Added comments to server.js, updated index.js ready for views implimentation

--- Source/frontend/static/js/index.js
+++ Source/frontend/static/js/index.js
@@ -1,1 +1,48 @@
-console.log("JS Loaded...")
+const navigateTo = url => { //prevents page refresh every time a link is pressed
+    history.pushState(null, null, url);
+    router();
+};
+
+
+const router = async () => { //async funtion to load page views
+    const routes = [
+        {path: "/404", view: () => console.log("Viewing 404") },
+        {path: "/", view: () => console.log("Viewing splashpage") }, // when the route = "/" runs function
+        {path: "/login", view: () => console.log("Viewing login") },
+        {path: "/register", view: () => console.log("Viewing register") },
+        {path: "/teams", view: () => console.log("Viewing teams") },
+        {path: "/newteam", view: () => console.log("Viewing newteam") },
+        {path: "/newmatch", view: () => console.log("Viewing newmatch") },
+    ];
+
+    //test each route for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    if (!match) { // if no match is found routes to a 404 page
+        match = {
+            route: routes[0],
+            isMatch: true
+        };
+    }
+
+    console.log(match.route.view());
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => { // when the DOM content is loaded router function is run
+    document.body.addEventListener("click", e => { //onclick in delegated listener, allows for more links to be added to injected html
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault(); //stops the default functionality of the element
+            navigateTo(e.target.href); //runs the nav function using the href as the url 
+        }
+    })
+    router();
+});
--- Source/server.js
+++ Source/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use("/static", express.static(path.resolve(__dirname,"frontend", "static")));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html")); //display inex.html in the frontend directory as the webpage
 });
 
-app.listen(process.env.PORT || 2040, () => console.log("Server running..."));
+app.listen(process.env.PORT || 2040, () => console.log("Server running...")); //outputs that the server is running in terminal and runs the server through port 2040
