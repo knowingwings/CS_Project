38785f7412cb114477e1cb31c58707b110696f34
Tom <thomas.lehuray@outlook.com>
2021-10-30 10:31:37 +0100

    Changed register to login in header and added already user link

--- Source/frontend/static/js/views/Register.js
+++ Source/frontend/static/js/views/Register.js
@@ -9,10 +9,11 @@ export default class extends AbstractView {
     async getHtml() { // html to be inserted
         return `
         <form class="box" id="loginForm" style="display:block;">
-            <h1>Login</h1>
+            <h1>Register</h1>
             <input type="text" name="" value="" placeholder="Username" id="usernameRegister">
             <input type="password" name="" value="" placeholder="Password" id="passwordRegister">
             <input type="button" name="" value="Register" onclick="register();">
+            <a href="/login" class="nav__link" data-link>Already have an account?</a>
         </form>
         `;
     }
