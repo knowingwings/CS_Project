106a86d69a27bfc186b5853ef9635b88a3904230
Tom <thomas.lehuray@outlook.com>
2021-11-03 13:51:20 +0000

    Initialised firestore connection, started login and register functions

--- Source/frontend/static/js/views/Login.js
+++ Source/frontend/static/js/views/Login.js
@@ -8,12 +8,12 @@ export default class extends AbstractView {
 
     async getHtml() { // html to be inserted
         return `
-        <form class="box" id="loginForm" style="display:block;">
+        <form class="box" id="loginForm" style="display:block;" action="auth" method="POST">
             <h1>Login</h1>
-            <input type="text" name="" value="" placeholder="Username" id="usernameLogin">
-            <input type="password" name="" value="" placeholder="Password" id="passwordLogin">
-            <input type="button" name="" value="Login">
-            <input type="button" name="" value="Register" data-link href="/register">
+            <input type="text" name="username" value="" placeholder="Username" id="usernameLogin">
+            <input type="password" name="password" value="" placeholder="Password" id="passwordLogin">
+            <input type="submit" name="" value="Login">
+            <a class="nav__link" data-link href="/register" id="registerLink" >Don't have an account?</a>
         </form>
         `;
     }
--- Source/server.js
+++ Source/server.js
@@ -1,7 +1,39 @@
+const { response } = require("express");
+const bcrypt = require('bcrypt');
 const express = require("express");
 const path = require("path");
+//const firebase = require('firebase');
+//require("firebase/firestore");
+const { initializeApp } = require("firebase/app");
+const { getFirestore } = require("firebase/firestore");
+
+
+
+
 
 const app = express();
+const saltRounds = 10;
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCjB13kHrESLrKIz2z3VwevLDvlLS4vX_8",
+
+    authDomain: "eq-manager-cs-proj.firebaseapp.com",
+  
+    projectId: "eq-manager-cs-proj",
+  
+    storageBucket: "eq-manager-cs-proj.appspot.com",
+  
+    messagingSenderId: "97347328589",
+  
+    appId: "1:97347328589:web:0dd1057a80d0136dbf1a04",
+  
+    measurementId: "G-0XL3Z59GEQ"
+  
+};
+
+initializeApp(firebaseConfig);
+const db = getFirestore();
+
 
 app.use("/static", express.static(path.resolve(__dirname,"frontend", "static")));
 
@@ -11,4 +43,18 @@ app.get("/*", (req, res) => {
 
 app.listen(process.env.PORT || 2040, () => console.log("Server running...")); //outputs that the server is running in terminal and runs the server through port 2040
 
-app.post('/auth')
+app.post('/auth', (req,response) => {
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+
+    if(username && password) {
+        if(username.length()>20 || password.length()>20){
+            response.send("Your Username or Password is too short")
+        }
+    }
+    else{
+        response.send("Please enter username and Password");
+        response.end();
+    }
+})
+
